bbbfd1f86a1e4bb4592061eacf7c2d93930d064a
jonasny92 <51217293+jonasny92@users.noreply.github.com>
2019-05-31 13:30:11 -0400

    Update writeSession.php

--- writeSession.php
+++ writeSession.php
@@ -1,8 +1,9 @@
 <?php
+// used to save login and id from index page to be retrieved by home page
 session_start();
-
 $inData = getRequestInfo();
 
+// save login and id in session variables
 $_SESSION["sessionLogin"] = $inData["login"];
 $_SESSION["sessionUserId"] = $inData["id"];
 
